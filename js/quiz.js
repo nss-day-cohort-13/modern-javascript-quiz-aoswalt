5207843fa1548b0fd8199fd872bc230290577e9c
Adam Oswalt <adam.oswalt@gmail.com>
2016-05-30 22:18:02 -0500

    confirm only if entries filled

--- js/quiz.js
+++ js/quiz.js
@@ -5,6 +5,17 @@
   Battledome.Factory.listBots(bot =>
     $(".setup .type").append(`<option value="${bot}">${bot}</option>`));
 
+  $(".name").click(isSetup);
+  $(".type").click(isSetup);
+
+  function isSetup() {
+    $(".confirm")[0].disabled =
+    !($(".bot1 .name").val() &&
+      $(".bot1 .type").val() &&
+      $(".bot2 .name").val() &&
+      $(".bot2 .type").val());
+  }
+
   $(".confirm").click(() => {
     $(".setup").hide();
     $(".combat").show();
